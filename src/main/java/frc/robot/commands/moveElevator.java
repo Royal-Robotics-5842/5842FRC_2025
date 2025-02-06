582d2d475d1b1a9cafd8f9aefd04f2603f1f46d3
KrishGupta25 <krishgupta2025@gmail.com>
2025-02-06 16:21:31 -0500

    krishCode

---
+++ src/main/java/frc/robot/commands/moveElevator.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Elevator;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class moveElevator extends Command {
+  /** Creates a new moveElevator. */
+  private Elevator elevator;
+  private double speed;
+  public moveElevator(Elevator elevator, double speed) {
+    this.elevator = elevator;
+    this.speed = speed;
+    addRequirements(elevator);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() 
+  {
+    System.out.println("Elevator up/down Started");
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() 
+  {
+    elevator.leftMotor.set(speed);
+    elevator.rightMotor.set(speed);
+
+    System.out.println("ENCODER" + elevator.rightMotor.getEncoder().getPosition());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) 
+  {
+    elevator.leftMotor.set(0);
+    elevator.rightMotor.set(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
--- src/main/java/frc/robot/commands/resetEverything.java
+++ src/main/java/frc/robot/commands/resetEverything.java
@@ -5,13 +5,16 @@
 package frc.robot.commands;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.SwerveSubsystem;
 
 public class resetEverything extends Command {
   private final SwerveSubsystem swerve;
-  public resetEverything(SwerveSubsystem swerve) {
+  private final Elevator elevator;
+  public resetEverything(SwerveSubsystem swerve, Elevator elevator) {
     this.swerve = swerve;
-    addRequirements(swerve);
+    this.elevator = elevator;
+    addRequirements(swerve, elevator);
   
   }
 
@@ -32,12 +35,14 @@ public class resetEverything extends Command {
     swerve.backLeft.setToAngle(0);
     swerve.backRight.setToAngle(0);
 
+    elevator.leftMotor.getEncoder().setPosition(0);
+/*
     swerve.frontLeft.setDrivePosition(0);
     swerve.frontRight.setDrivePosition(0);
     swerve.backLeft.setDrivePosition(0);
     swerve.backRight.setDrivePosition(0);
-
-    swerve.resetOdemetry(new Pose2d(0, 0, swerve.gyro.getRotation2d()));
+*/
+    swerve.resetOdometry(new Pose2d(0,0, swerve.gyro.getRotation2d()));
   }
   
   // Called once the command ends or is interrupted.
