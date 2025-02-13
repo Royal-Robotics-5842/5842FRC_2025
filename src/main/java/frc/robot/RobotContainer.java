7776e83096df0fd967b204b5fa8ede730a35e050
KrishGupta25 <krishgupta2025@gmail.com>
2025-02-13 11:30:04 -0500

    limelight starting

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,21 +4,22 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OIConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ShootCoral;
 import frc.robot.commands.SwerveDriveJoystick;
+import frc.robot.commands.SwerveDriveJoystickLimeLight;
 import frc.robot.commands.elevPID;
 import frc.robot.commands.moveElevator;
 import frc.robot.commands.resetEverything;
 import frc.robot.subsystems.CoralShooter;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.SwerveSubsystem;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -44,23 +45,19 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    
+   
     swerveSubsystem.setDefaultCommand(new SwerveDriveJoystick(
       swerveSubsystem,
       () -> -driverJoytick.getRawAxis(OIConstants.kDriverYAxis), // Forward/Back DO NOT TOUCH
       () -> -driverJoytick.getRawAxis(OIConstants.kDriverXAxis), // Left/Right
       () -> -driverJoytick.getRawAxis(OIConstants.kDriverRotAxis),
       () -> !m_driverController.y().getAsBoolean()));
-     
+    
       SmartDashboard.putBoolean("Field Centric", !m_driverController.y().getAsBoolean());
       SmartDashboard.putNumber("Robot Pitch", swerveSubsystem.gyro.getPitch());
 
     configureBindings();
     
-    SmartDashboard.putNumber("Turn BL", swerveSubsystem.backLeft.getTurningPosition());
-    SmartDashboard.putNumber("Turn FL", swerveSubsystem.frontLeft.getTurningPosition());
-    SmartDashboard.putNumber("Turn BR", swerveSubsystem.backRight.getTurningPosition());
-    SmartDashboard.putNumber("Turn FR", swerveSubsystem.frontRight.getDrivePosition());
     
     
   }
@@ -77,15 +74,21 @@ public class RobotContainer {
    */
   private void configureBindings() {
     m_driverController.rightTrigger().onTrue((new resetEverything(swerveSubsystem,elevator)).withTimeout(0.5));
-    m_driverController.a().toggleOnTrue(new moveElevator(elevator, 0.75));
+    
+    m_driverController.a().whileTrue(new SwerveDriveJoystickLimeLight(
+      swerveSubsystem,
+      () -> -driverJoytick.getRawAxis(OIConstants.kDriverYAxis), // Forward/Back DO NOT TOUCH
+      () -> -driverJoytick.getRawAxis(OIConstants.kDriverXAxis), // Left/Right
+      () -> !m_driverController.y().getAsBoolean()));
+
     m_driverController.y().toggleOnTrue(new moveElevator(elevator, 0.05));
     m_driverController.x().toggleOnTrue(new moveElevator(elevator, -0.75));
     m_driverController.b().toggleOnTrue(new moveElevator(elevator, 0));
 
-    m_driverController.povUp().toggleOnTrue(new elevPID(elevator, 183));
+    m_driverController.povUp().toggleOnTrue(new elevPID(elevator, 178));
     m_driverController.povLeft().toggleOnTrue(new elevPID(elevator, 103));
     m_driverController.povRight().toggleOnTrue(new elevPID(elevator, 47));
-    m_driverController.povDown().toggleOnTrue(new elevPID(elevator, 3));
+    m_driverController.povDown().toggleOnTrue(new elevPID(elevator, 1));
     
 
     m_driverController.leftBumper().toggleOnTrue(new ShootCoral(coral, 0.10));
--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -12,10 +12,13 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.Constants.DriveConstants;
 
 //Making the SwerveModules with all the different paramters
@@ -64,6 +67,14 @@ public class SwerveSubsystem extends SubsystemBase {
     
     public final Field2d m_field = new Field2d(); //For Glass
 
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight-lite");
+    NetworkTableEntry tx = table.getEntry("tx");
+    NetworkTableEntry ty = table.getEntry("ty");
+    NetworkTableEntry ta = table.getEntry("ta");
+
+  
+
+
     public SwerveSubsystem() {   
       new Thread(() -> {
         try {
@@ -72,7 +83,6 @@ public class SwerveSubsystem extends SubsystemBase {
         } catch (Exception e) {
         }
     }).start();
-
 }
 
 
@@ -122,13 +132,45 @@ public class SwerveSubsystem extends SubsystemBase {
     backRight.setDesiredState(desiredStates[3]);
   }
 
+  public double limelight_aim_proportional()
+  {    
+    // kP (constant of proportionality)
+    // this is a hand-tuned number that determines the aggressiveness of our proportional control loop
+    // if it is too high, the robot will oscillate around.
+    // if it is too low, the robot will never reach its target
+    // if the robot never turns in the correct direction, kP should be inverted.
+    double kP = .02;
 
+    // tx ranges from (-hfov/2) to (hfov/2) in degrees. If your target is on the rightmost edge of 
+    // your limelight 3 feed, tx should return roughly 31 degrees.
+    double targetingAngularVelocity = table.getEntry("tx").getDouble(0) * kP;
+
+    // convert to radians per second for our drive method
+    targetingAngularVelocity *= Math.PI;
+
+    //invert since tx is positive when the target is to the right of the crosshair
+    targetingAngularVelocity *= -1.0;
+
+    return targetingAngularVelocity;
+  }
+
+  public double limelight_range_proportional()
+  {    
+    double kP = 0.05;
+    double targetingForwardSpeed = ty.getDouble(0) * kP;
+    targetingForwardSpeed *= Constants.DriveConstants.kTeleDriveMaxSpeedMetersPerSecond;
+    targetingForwardSpeed *= -1.0;
+    return targetingForwardSpeed;
+  }
   @Override
 
   public void periodic() {
 
     //ALL OF THIS HAPPENS 5 TIMES A SECOND
     //Just stuff for debugging and for Driver!
+    double limelight_x = tx.getDouble(0.0);
+    double limelight_y = ty.getDouble(0.0);
+    double limelight_area = ta.getDouble(0.0);
 
       odometer.update(getRotation2d(), new SwerveModulePosition[] {
       frontLeft.getPosition(),
@@ -140,6 +182,16 @@ public class SwerveSubsystem extends SubsystemBase {
     SmartDashboard.putBoolean("AutoBalance?", gyro.getPitch() <= 1);
     SmartDashboard.putData(m_field);
     SmartDashboard.putNumber("Pitch Of Robot", gyro.getPitch());
+
+    
+    SmartDashboard.putNumber("Turn BL", backLeft.getTurningPosition());
+    SmartDashboard.putNumber("Turn FL", frontLeft.getTurningPosition());
+    SmartDashboard.putNumber("Turn BR", backRight.getTurningPosition());
+    SmartDashboard.putNumber("Turn FR", frontRight.getTurningPosition());
+
+    SmartDashboard.putNumber("LimelightX", limelight_x*Math.PI);
+    SmartDashboard.putNumber("LimelightY", limelight_y);
+    SmartDashboard.putNumber("LimelightArea", limelight_area);
     
     m_field.setRobotPose(odometer.getPoseMeters());
   }
